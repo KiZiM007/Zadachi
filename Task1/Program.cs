519891718a069758b2995943cafaa494a6ced22f
Kim <kim_konstantinov@bk.ru>
2022-08-07 18:31:43 +0300

    Создал очередную программу Task3

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -12,11 +12,11 @@ int num1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число:");
 int num2 = int.Parse(Console.ReadLine());
 
-if(num2 == num1 * num1)
+if (num2 == num1 * num1)
 {
-Console.Write("Да!");
+    Console.Write($"{num1} в квадрате ровняется {num2}");
 }
 else
 {
-Console.Write("Нет(");
+    Console.Write($"{num1} в квадрате не ровняется {num2}");
 }
---
+++ Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+Console.Write("Введите число от 1 до 7: ");
+int num = int.Parse(Console.ReadLine());
+if (num == 1)
+{
+    Console.Write("Сегодня понедельник!");
+}
+else if (num == 2)
+{
+    Console.Write("Сегодня вторник!");
+}
+else if (num == 3)
+{
+    Console.Write("Сегодня среда!");
+}
+else if (num == 4)
+{
+    Console.Write("Сегодня четверг!");
+}
+else if (num == 5)
+{
+    Console.Write("Сегодня пятница!");
+}
+else if (num == 6)
+{
+    Console.Write("Сегодня суббота!");
+}
+else if (num == 7)
+{
+    Console.Write("Сегодня воскресенье!");
+}
+else
+{
+    Console.Write("Ошибка: ввели неверное значение, пожалуйста, введите число от 1 до 7: ");
+}
