28e2fabade48db7dc22c63203ae7cd1cd2e408a3
Kim <kim_konstantinov@bk.ru>
2022-08-10 17:34:18 +0300

    Save END2

---
+++ Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+
+
+int random = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {random}");
+int Metod(int ran)
+{
+int number1 = ran / 100;
+int number3 = ran % 10;
+return number1*10+number3;
+}
+int result = Metod(random);
+Console.Write($"Число {random} без средней цифры -> {result}");
