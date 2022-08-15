3ed91d552b2a6278ea49769e8ff0e322df858325
Kim <kim_konstantinov@bk.ru>
2022-08-15 18:44:31 +0300

    Save Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int size = 1;
+// while (num >= size)
+// {
+//     Console.WriteLine($"{size}->{size * size}");
+//     size ++;
+// }
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int size = 1;
+void Tabl(int number)
+{
+    while (num >= size)
+    {
+        Console.Write(size * size);
+        size = size + 1;
+    }
+}
+Console.WriteLine(Tabl(num));
--- Task6/Program.cs
+++ Task6/Program.cs
@@ -8,6 +8,19 @@
 
 
 
+// int random = new Random().Next(100, 1000);
+// Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {random}");
+// int Metod(int ran)
+// {
+// int number1 = ran / 100;
+// int number3 = ran % 10;
+// return number1*10+number3;
+// }
+// Console.Write($"Число {random} без средней цифры -> {Metod(random)}");
+
+
+
+
 int random = new Random().Next(100, 1000);
 Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {random}");
 int Metod(int ran)
@@ -16,5 +29,5 @@ int number1 = ran / 100;
 int number3 = ran % 10;
 return number1*10+number3;
 }
-int result = Metod(random);
-Console.Write($"Число {random} без средней цифры -> {result}");
+Console.Write($"Число {random} без средней цифры -> {Metod(random)}");
+
